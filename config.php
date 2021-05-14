1937ef5d156575796619d5e96c5b6477b6393385
PareshDevlekar <78142905+PareshDevlekar@users.noreply.github.com>
2021-05-14 14:07:54 +0530

    edit config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect('localhost', 'root', '', 'sparks_bank');
+	$conn = mysqli_connect('localhost', 'id16814383_paresh', '>qTRu\|h{v4ZqYY6', 'id16814383_sparks_bank_db');
 	if (!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
 	}
